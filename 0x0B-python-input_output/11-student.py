86f217160b7a3daa80901b9a967a95cd7fd3f83a
koolpedro <pantakorah@gmail.com>
2023-01-12 01:03:52 -0800

    0x0B python i/O tasks

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,7 +14,7 @@ class Student:
             age (int): The age of the student.
         """
         self.first_name = first_name
-        self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self, attrs=None):
@@ -25,16 +25,16 @@ class Student:
 
         Args:
             attrs (list): (Optional) The attributes to represent.
-         """
-         if (type(attrs) == list and
-                 all(type(ele) == str for ele in attrs)):
-             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-         return self.__dict__
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
-     def reload_from_json(self, json):
-         """Replace all attributes of the Student.
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
 
-         Args:
+        Args:
             json (dict): The key/value pairs to replace attributes with.
         """
         for k, v in json.items():
